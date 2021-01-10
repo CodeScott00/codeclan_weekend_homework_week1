bbd451ee3e26489c636d8564535df8cd25eb5c9e
Skygod <dougal_00@hotmail.com>
2021-01-10 18:55:16 +0000

    first homework attempt

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,21 @@
-# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop['admin']['total_cash']
+
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop['admin']['total_cash'] += amount
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, sold):
+    pet_shop ['admin']['pets_sold'] += sold
+
+def get_stock_count(pet_shop,stock):
+    for stock in pet_shop:
+        if stock == ['breed_type']
+    return stock
+
+
